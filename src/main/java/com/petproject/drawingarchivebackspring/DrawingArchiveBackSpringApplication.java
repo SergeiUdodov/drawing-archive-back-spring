47960e98d532c3fda9a7927c4c84ba5175de9458
sergei <s_udodov@bk.ru>
2023-05-02 13:16:55 +0300

    initial commit

---
+++ src/main/java/com/petproject/drawingarchivebackspring/DrawingArchiveBackSpringApplication.java
@@ -0,0 +1,13 @@
+package com.petproject.drawingarchivebackspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrawingArchiveBackSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrawingArchiveBackSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petproject/drawingarchivebackspring/DrawingArchiveBackSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petproject.drawingarchivebackspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrawingArchiveBackSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
